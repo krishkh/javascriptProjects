77274f60c57ec2e7c6f9f74e95b465c22c9e9ed4
Krish <krishkhanna00@gmail.com>
2024-04-05 16:15:09 +0530

    adding new project called tictactoe

--- rock_paper_scissors/logic.js
+++ rock_paper_scissors/logic.js
@@ -1,3 +1,14 @@
-function name() {
-  console.log("hi");
+const imageContainer = document.querySelector("#images");
+const images = document.querySelectorAll(".image");
+
+
+const game = (userChoice) =>{
+  
 }
+
+
+images.forEach((image) => {
+  image.addEventListener("click", () => {
+    console.log("you3 have clicked the,",image.id);
+  });
+});
